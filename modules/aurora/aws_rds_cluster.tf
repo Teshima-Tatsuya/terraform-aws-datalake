8e6fb2a1b5972db9c6eee06a50d39cbcaac2e375
Tatsuya Teshima <terima666tatsuya@gmail.com>
2023-11-16 15:18:31 +0900

    add aurora

--- modules/aurora/aws_rds_cluster.tf
+++ modules/aurora/aws_rds_cluster.tf
@@ -4,6 +4,7 @@ resource "aws_rds_cluster" "main" {
     master_password = "Passw0rd"
     db_subnet_group_name = aws_db_subnet_group.main.id
     engine = "aurora-mysql"
+    engine_version = "8.0.mysql_aurora.3.04.1"
     engine_mode = "serverless"
     vpc_security_group_ids = [var.vpc.sg.teshima-sg-rds.id]
     db_cluster_parameter_group_name = aws_rds_cluster_parameter_group.main.name
