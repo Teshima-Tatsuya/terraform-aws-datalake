b33d253eccd71f11bdb75f4ba7614d06981b8dd7
Tatsuya Teshima <terima666tatsuya@gmail.com>
2023-11-16 17:20:53 +0900

    lambda

--- modules/iam/aws_iam_role.tf
+++ modules/iam/aws_iam_role.tf
@@ -18,6 +18,6 @@ data "aws_iam_policy_document" "assume_role" {
 }
 
 resource "aws_iam_role" "lambda-role" {
-  name = "lambda-role"
+  name = "lambda-role-for-datalake"
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
