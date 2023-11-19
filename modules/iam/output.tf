b253909a9ba0294f6a3fc75d56f9ac23c760d4e2
Tatsuya Teshima <terima666tatsuya@gmail.com>
2023-11-19 15:08:31 +0900

    temp

--- modules/iam/output.tf
+++ modules/iam/output.tf
@@ -3,5 +3,6 @@ output "iam" {
     "role" = aws_iam_role.web-server
     "role_lambda" = aws_iam_role.lambda-role
     "profile" = aws_iam_instance_profile.web-server
+    "temp" = data.aws_iam_policy_document.firehose-to-s3.json
   }
 }
