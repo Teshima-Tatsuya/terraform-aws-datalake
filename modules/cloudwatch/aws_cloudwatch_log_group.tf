59ba435ed2869429e7fbcbb94f6f618a67aad47f
Tatsuya Teshima <terima666tatsuya@gmail.com>
2023-11-25 17:20:22 +0900

    add loggroup

--- modules/cloudwatch/aws_cloudwatch_log_group.tf
+++ modules/cloudwatch/aws_cloudwatch_log_group.tf
@@ -11,4 +11,5 @@ resource "aws_cloudwatch_log_group" "main" {
     for_each = toset(local.name)
 
     name = "/aws/rds/cluster/${var.aurora.cluster_identifier}/${each.value}"
+    retention_in_days = 90
 }
