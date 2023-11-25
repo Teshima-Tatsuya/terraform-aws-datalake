3574daa2328b28d0a6a4efce073847793fc13f94
Tatsuya Teshima <terima666tatsuya@gmail.com>
2023-11-25 18:26:31 +0900

    change prefix

--- variables_kinesis.tf
+++ variables_kinesis.tf
@@ -2,19 +2,19 @@ variable "firehose" {
   default = {
     "audit" = {
       name = "datalake-aurora-audit-logs"
-      prefix = "datalake/firehose/aurora_audit_logs"
+      prefix = "datalake/firehose/aurora_audit_logs/"
     },
     "error" = {
       name = "datalake-aurora-error-logs"
-      prefix = "datalake/firehose/aurora_error_logs"
+      prefix = "datalake/firehose/aurora_error_logs/"
     },
     "general" = {
       name = "datalake-aurora-general-logs"
-      prefix = "datalake/firehose/aurora_general_logs"
+      prefix = "datalake/firehose/aurora_general_logs/"
     },
     "slowquery" = {
       name = "datalake-aurora-slowquery-logs"
-      prefix = "datalake/firehose/aurora_slowquery_logs"
+      prefix = "datalake/firehose/aurora_slowquery_logs/"
     },
   }
 }
